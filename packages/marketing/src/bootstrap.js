b95b4172ace0543271a0dae19b71cb7618e516b7
Mahta <mahta@sprintasia.co.id>
2023-07-06 21:54:07 +0700

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//mount function to start up the app
+const mount = (el) =>{
+    ReactDOM.render(
+        <App />,el
+    );
+};
+
+//if we are in development and in solution, call mount immediately
+if(process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+export { mount };
