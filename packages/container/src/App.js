9d0ea6ce9b6b4e39f068a3e186e81803005c2eea
Mahta <mahta@sprintasia.co.id>
2023-07-13 12:19:52 +0700

    update css fixing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 //import { mount } from 'marketing/MarketingApp';
-
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 //console.log(mount);
 export default () =>{
     return (
     <BrowserRouter>
-        <div> 
-            <Header />
-            <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div> 
+                <Header />
+                <MarketingApp />
+            </div>
+        </StylesProvider>
     </BrowserRouter>
     );
 };
