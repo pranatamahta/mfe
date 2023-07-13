1d1503fe68c42bdbaca51f93abefed6b2c627866
Mahta <mahta@sprintasia.co.id>
2023-07-13 11:23:29 +0700

    update header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 //import { mount } from 'marketing/MarketingApp';
 
 //console.log(mount);
 export default () =>{
-    return <div> 
-    <h1>
-        Hi there!!!,
-    </h1>
-    <hr />
-    <MarketingApp />
-    </div>
-}
+    return (
+    <BrowserRouter>
+        <div> 
+            <Header />
+            <MarketingApp />
+        </div>
+    </BrowserRouter>
+    );
+};
