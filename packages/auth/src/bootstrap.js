a24ee87b627d916aede698f3948471a80c15ba26
Mahta <mahta@sprintasia.co.id>
2023-07-14 14:39:27 +0700

    add sub app

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,17 +4,20 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 //mount function to start up the app
-const mount = (el,{ onNavigate ,defaultHistory}) =>{
-    const history = defaultHistory || createMemoryHistory();
+const mount = (el,{ onSignIn, onNavigate ,defaultHistory, initialPath}) =>{
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries:[initialPath]
+    });
     if(onNavigate){
         history.listen(onNavigate);
     }
     ReactDOM.render(
-        <App history={history}/>,el
+        <App onSignIn={onSignIn} history={history}/>,el
     );
     return {
         onParentNavigate({pathname:nextPathname}) {
             const {pathname} = history.location;
+            console.log(nextPathname);
             if(pathname !== nextPathname){
                 history.push(nextPathname);
             }
@@ -24,7 +27,7 @@ const mount = (el,{ onNavigate ,defaultHistory}) =>{
 
 //if we are in development and in solution, call mount immediately
 if(process.env.NODE_ENV === 'development'){
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
     if(devRoot){
         mount(devRoot,{ defaultHistory:createBrowserHistory()});
     }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch,Route, BrowserRouter } from "react-router-dom";
+import { Switch,Route, BrowserRouter, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from './components/Landing'
@@ -9,13 +9,13 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma',
 });
 
-export default () =>{
+export default ({history}) =>{
     return <div>
         <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+            <Router history={history}>
                 <Route exact path="/pricing" component={Pricing}/>
                 <Route exact path="/" component={Landing}/>
-            </BrowserRouter>
+            </Router>
         </StylesProvider>
     </div>
 };
